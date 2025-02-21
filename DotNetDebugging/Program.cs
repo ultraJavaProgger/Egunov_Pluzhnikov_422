9da36982878db3a02129ee86abb13e693404c053
Al_tushka <Al_tushka@HOME-PC>
2025-02-21 15:36:29 +0300

    galaxy task

--- DotNetDebugging/Program.cs
+++ DotNetDebugging/Program.cs
@@ -1,18 +1,76 @@
-﻿int result = Fibonacci(5);
-Console.WriteLine(result);
+﻿using System;
+using System.Collections.Generic;
 
-static int Fibonacci(int n)
+namespace ConsoleApp_FirstApp
 {
-    int n1 = 0;
-    int n2 = 1;
-    int sum;
-
-    for (int i = 2; i <= n; i++)
+    class Program
     {
-        sum = n1 + n2;
-        n1 = n2;
-        n2 = sum;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Galaxy News!");
+            IterateThroughList();
+            Console.ReadKey();
+        }
+
+        private static void IterateThroughList()
+        {
+            var theGalaxies = new List<Galaxy>
+        {
+            new Galaxy() { Name="Tadpole", MegaLightYears=400, GalaxyType=new GType('S')},
+            new Galaxy() { Name="Pinwheel", MegaLightYears=25, GalaxyType=new GType('S')},
+            new Galaxy() { Name="Cartwheel", MegaLightYears=500, GalaxyType=new GType('L')},
+            new Galaxy() { Name="Small Magellanic Cloud", MegaLightYears=.2, GalaxyType=new GType('I')},
+            new Galaxy() { Name="Andromeda", MegaLightYears=3, GalaxyType=new GType('S')},
+            new Galaxy() { Name="Maffei 1", MegaLightYears=11, GalaxyType=new GType('E')}
+        };
+
+            foreach (Galaxy theGalaxy in theGalaxies)
+            {
+                Console.WriteLine(theGalaxy.Name + "  " + theGalaxy.MegaLightYears + ",  " + theGalaxy.GalaxyType);
+            }
+
+            // Expected Output:
+            //  Tadpole  400,  Spiral
+            //  Pinwheel  25,  Spiral
+            //  Cartwheel, 500,  Lenticular
+            //  Small Magellanic Cloud .2,  Irregular
+            //  Andromeda  3,  Spiral
+            //  Maffei 1,  11,  Elliptical
+        }
     }
 
-    return n == 0 ? n1 : n2;
+    public class Galaxy
+    {
+        public string Name { get; set; }
+
+        public double MegaLightYears { get; set; }
+        public object GalaxyType { get; set; }
+
+    }
+
+    public class GType
+    {
+        public GType(char type)
+        {
+            switch (type)
+            {
+                case 'S':
+                    MyGType = Type.Spiral;
+                    break;
+                case 'E':
+                    MyGType = Type.Elliptical;
+                    break;
+                case 'l':
+                    MyGType = Type.Irregular;
+                    break;
+                case 'L':
+                    MyGType = Type.Lenticular;
+                    break;
+                default:
+                    break;
+            }
+        }
+        public object MyGType { get; set; }
+        private enum Type { Spiral, Elliptical, Irregular, Lenticular }
+    }
 }
