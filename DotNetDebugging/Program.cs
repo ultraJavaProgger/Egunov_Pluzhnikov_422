7571104c0e01e7280ab5e7737c2ea9cec96a91b5
Al_tushka <Al_tushka@HOME-PC>
2025-02-21 16:13:28 +0300

    fred smith

--- DotNetDebugging/Program.cs
+++ DotNetDebugging/Program.cs
@@ -1,76 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace ConsoleApp_FirstApp
+class ArrayExample
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
+        string name = "";
+        int[] a = new int[10];
+        for (int i = 0; i < letters.Length; i++)
         {
-            Console.WriteLine("Welcome to Galaxy News!");
-            IterateThroughList();
-            Console.ReadKey();
+            name += letters[i];
+            a[i] = i + 1;
+            SendMessage(name, a[i]);
         }
-
-        private static void IterateThroughList()
-        {
-            var theGalaxies = new List<Galaxy>
-        {
-            new Galaxy() { Name="Tadpole", MegaLightYears=400, GalaxyType=new GType('S')},
-            new Galaxy() { Name="Pinwheel", MegaLightYears=25, GalaxyType=new GType('S')},
-            new Galaxy() { Name="Cartwheel", MegaLightYears=500, GalaxyType=new GType('L')},
-            new Galaxy() { Name="Small Magellanic Cloud", MegaLightYears=.2, GalaxyType=new GType('I')},
-            new Galaxy() { Name="Andromeda", MegaLightYears=3, GalaxyType=new GType('S')},
-            new Galaxy() { Name="Maffei 1", MegaLightYears=11, GalaxyType=new GType('E')}
-        };
-
-            foreach (Galaxy theGalaxy in theGalaxies)
-            {
-                Console.WriteLine(theGalaxy.Name + "  " + theGalaxy.MegaLightYears + ",  " + theGalaxy.GalaxyType.MyGType);
-            }
-
-            // Expected Output:
-            //  Tadpole  400,  Spiral
-            //  Pinwheel  25,  Spiral
-            //  Cartwheel, 500,  Lenticular
-            //  Small Magellanic Cloud .2,  Irregular
-            //  Andromeda  3,  Spiral
-            //  Maffei 1,  11,  Elliptical
-        }
+        Console.ReadKey();
     }
 
-    public class Galaxy
+    static void SendMessage(string name, int msg)
     {
-        public string Name { get; set; }
-
-        public double MegaLightYears { get; set; }
-        public GType GalaxyType { get; set; }
-
-    }
-
-    public class GType
-    {
-        public GType(char type)
-        {
-            switch (type)
-            {
-                case 'S':
-                    MyGType = Type.Spiral;
-                    break;
-                case 'E':
-                    MyGType = Type.Elliptical;
-                    break;
-                case 'I':
-                    MyGType = Type.Irregular;
-                    break;
-                case 'L':
-                    MyGType = Type.Lenticular;
-                    break;
-                default:
-                    break;
-            }
-        }
-        public object MyGType { get; set; }
-        private enum Type { Spiral, Elliptical, Irregular, Lenticular }
+        Console.WriteLine("Hello, " + name + "! Count to " + msg);
     }
 }
